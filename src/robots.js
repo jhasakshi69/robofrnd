954e203d9f33e3a51a1d144129430c0942e9c157
jhasakshi69 <64399052+jhasakshi69@users.noreply.github.com>
2020-06-04 11:17:34 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots =[
+{
+	id: 1,
+	name: 'william shakshpear',
+	email: 'william.shakshpear@gmail.com'
+},
+{
+	id: 2,
+	name: 'geogre niogia',
+	email: 'geogre.niogia@gmail.com'
+},
+{
+	id: 3,
+	name: 'chetan bhagat',
+	email: 'chetan.bhagat@gmail.com'
+},
+{
+	id: 4,
+	name: 'shubham jha',
+	email: 'shubham.jha@gmail.com'
+},
+{
+	id: 5,
+	name: 'akancha sagar',
+	email: 'akancha.sagar@gmail.com'
+},
+{
+	id: 6,
+	name: 'sakshi jha',
+	email: 'sakshi.jha@gmail.com'
+},
+{
+	id: 7,
+	name: 'sonali mishra',
+	email: 'sonali.mishra@gmail.com'
+},
+{
+	id: 8,
+	name: 'richa jha',
+	email: 'richa.jha@gmail.com'
+},
+{
+	id: 9,
+	name: 'tanu jha',
+	email: 'tanu.jha@gmail.com'
+},
+{
+	id: 10,
+	name: 'anku jha',
+	email: 'anku.jha@gmail.com'
+},
+]
